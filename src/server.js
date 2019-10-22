e5ffc002fc383834d20e7a6bb2855be42e7e7c25
Nick <k1889573@kcl.ac.uk>
2019-10-22 19:45:12 +0100

    initial server config

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.listen(5000, () => {
+    console.warn('Server is now listening on port 5000');
+})
